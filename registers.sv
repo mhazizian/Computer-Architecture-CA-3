7a493345f7a8edec3b1103c482a76759bfe394b8
moridi <m.moridi.2009@gmail.com>
2018-05-10 10:58:41 +0430

    Delete unuseful registers

--- registers.sv
+++ registers.sv
@@ -13,38 +13,3 @@ module register #(parameter integer WORD_LENGTH) (clk, rst, ld, in, out);
 	end
 	
 endmodule
-
-// PC register
-
-module pc_register (clk, rst, ld, cen, in, out);
-
-	input clk, rst, ld, cen;
-	input[12:0] in;
-	output logic [12:0] out;
-	
-	always@(posedge clk, posedge rst)
-	begin
-		if(cen) out <= out + 1;
-		if(ld) 	out <= in;
-		if(rst)	out <= 13'b0;
-	end
-	
-endmodule
-
-//  TR register
-
-module tr_register (clk, rst, ld_8, ld_5, in_8, in_5, out);
-
-	input clk, rst, ld_8, ld_5;
-	input [7:0] in_8;
-	input [4:0] in_5;
-	output logic [12:0] out;
-	
-	always@(posedge clk, posedge rst)
-	begin
-		if(ld_8) 	out[7:0] 	<= in_8;
-		if(ld_5) 	out[12:8] 	<= in_5;
-		if(rst)		out 		<= 13'b0;
-	end
-	
-endmodule
